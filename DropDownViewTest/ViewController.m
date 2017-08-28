9e245455fe082f7d4a5b55f0ae0f3b0c807ef39a
yang <2922324499@qq.com>
2017-08-28 15:20:21 +0800

    ..

--- DropDownViewTest/ViewController.m
+++ DropDownViewTest/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "YDropDownView.h"
 
-@interface ViewController ()
+@interface ViewController ()<YDropDownViewDelegate>
 
 @end
 
@@ -17,8 +18,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    YDropDownView *dview = [[YDropDownView alloc] initWithFrame:CGRectMake(0, 100, 200, 40)];
+    dview.backgroundColor = [UIColor orangeColor];
+    dview.defaultStr = @"老虎";
+    dview.listData = @[@"老虎",@"狮子",@"大象",@"熊猫",@"企鹅",@"大雁",@"喜鹊"];
+    dview.delegate = self;
+    dview.maxHeight = 300;
+    dview.menuBlock = ^(NSInteger i ,NSString *chooseStr){
+        
+    };
+    [self.view addSubview:dview];
 }
 
+#pragma mark -DropDownDelegate
+- (void)showShadowViewWithBool:(BOOL)isShow {
+    if (isShow) {
+        //添加手势
+    }else {
+        //不添加
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
